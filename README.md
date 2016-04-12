403b8c68bc0f05a722c6ed82e20aadc8df3787f2
Philip Hutchins <philip@storj.io>
2016-04-12 10:44:27 -0400

    works.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # storj-cdnproxy
+
+## Setup
+```
+npm install
+```
+
+## Configure
+Create a `.env` file in the root directory of the repository. Add your bucket hash and apiKey as follows.
+
+```
+API_KEY=yourapikeyhere
+BUCKET=yourbuckethashhere
+```
+
+## Run
+Currently to start, run the following...
+
+```
+node index.js
+```
--- app/routes/getbyhash.js
+++ app/routes/getbyhash.js
@@ -18,56 +18,21 @@ module.exports = function(router) {
 
   router.route('/:imageHash')
   .get(function(req, res) {
-    console.log("Image Hash: " + req.params.imageHash);
+    console.log("Request for image hash: " + req.params.imageHash);
 
     // Keep track of the bucket ID and file hash
     var filehash = req.params.imageHash;
 
     client.createToken(bucket, 'PULL').then(function(token) {
       // Fetch the file pointer list
-      console.log("Returning getFilePointers");
       return client.getFilePointer(bucket, token.token, filehash);
     }).then(function(pointers) {
-      // Open download stream from network and a writable file stream
-      console.log("Got pointers, sending image to client");
-
-      var objectStream = fs.createWriteStream();
       var object = client.resolveFileFromPointers(pointers)
 
-      console.log("Got object from resolve file pointers");
-
       object.pipe(res);
-      //res.writeHead(200, {'Content-Type': 'image/jpeg' });
-
-      console.log("Piped image to var");
-
-      // Need to create some sort of stream object here
-      //var destination = fs.createWriteStream('<write_file_to_path>');
-
-      console.log("Wrote HEAD");
-
-      //Need to pipe the stream into a string here
-      //download.pipe(res);
-      return res.end();
     });
   })
   .post(function(req, res, next) {
-    // Extract the ping data from the request
-    var body = req.body;
-    //var pingDataJSON = JSON.parse(body);
-    var pingDataJSON = body;
-
-    console.log("[POST][getObject] Request.body: ", body);
-
-    Ping.create(pingDataJSON, function(err) {
-      if (err) {
-        console.log("[POST][PING] Failed to save ping: " + err);
-        return res.sendStatus(500);
-      }
-
-      console.log("[POST][PING] Ping created!");
-
-      return res.sendStatus(200);
-    });
+    return res.sendStatus(200);
   });
 };
